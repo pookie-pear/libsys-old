58b39069f9faaf7e5d2542133c5e040f75afbb4f
pookie-pear <vestok33@gmail.com>
2026-07-15 23:49:32 -0700

    minor abt change
    yeah

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
         </h1>
         <p style={{ color: 'var(--text-muted)', fontSize: '1.1rem', marginBottom: '48px', lineHeight: '1.6' }}>
           my personal media ecosystem. IF youre admin you can manage books, movies, games, and more in one unified interface. contant me if youre really into adding stuff to collections n shi. other than that feel free to add stuff to your own wishlist. (as long as youre logged in, and dont spam logins imma ban yall)
-        </p>
+        copyright @suvam kumar 2026</p>
 
         <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))', gap: '24px' }}>
           <a 
